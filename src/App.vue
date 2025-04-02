3cf033865c98c60be20b0b1dd42c0468319bfcc5
Maceo <maceod23@gmail.com>
2025-04-02 10:27:36 +0200

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,32 @@
+<script setup>
+import { computed } from "vue";
+import { useRouter, useRoute } from "vue-router";
+
+const router = useRouter();
+const route = useRoute();
+console.log(route.path);
+
+function logout() {
+  localStorage.removeItem("user");
+  router.push("/register");
+}
+
+const showNavbar = computed(() => !["/login", "/register"].includes(route.path));
+</script>
+
 <template>
-  Navbar
+  <header v-if="showNavbar" class="navbar">
+    <button @click="logout">Se déconnecter</button>
+  </header>
   <main>
     <div class="container">
       <RouterView />
     </div>
   </main>
 </template>
+
+<style scoped>
+.navbar {
+  padding: 1rem;
+}
+</style>
